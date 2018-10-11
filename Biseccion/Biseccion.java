6a0329364e6ab72c3e9ad8bdce0ae043b835ca9a
guilogar <jdkdejava@gmail.com>
2018-10-11 12:07:16 +0200

    Refactorización del código del método de la Bisección.

--- Biseccion/Biseccion.java
+++ Biseccion/Biseccion.java
@@ -1,26 +1,19 @@
 
 public class Biseccion {
+    static double f2(double x) { return ( x * x ) - 5; }
     
-    static double f2(double x){return (x*x)-5;}
-    
-    public static void main(String args[]){
-        double a = 2, b = 3, c = 0, f_a, f_c;
-        double m = 0.1;
+    public static void main(String args[])
+    {
+        double a = 2, b = 3, c = 0, f_a, f_c, m = 0.1;
         
         while(b - a > m)
         {
-            c = (a + b) / 2;
-            f_a = f2(a); f_c = f2(c);
+            c = (a + b) / 2; f_a = f2(a); f_c = f2(c);
             
-            if(f_a * f_c < 0)
-            {
-                b = c;
-                System.out.println("Cambio en el interalo");
-            }
-            else if(f_a * f_c > 0) a = c;
-            else a=b=c;
+            if(f_a * f_c < 0) b = c;
+            else { if(f_a * f_c > 0) a = c; else a=b=c; }
         }
         
-        System.out.println(c+" es solucion");
+        System.out.println(c + " es solucion");
     }
 }
